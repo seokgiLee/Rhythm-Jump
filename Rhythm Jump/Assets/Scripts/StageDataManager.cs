44ce4b79efbaa235a182215cb6f6e9bb93c6e064
seokgiLee <plot_lee@naver.com>
2022-03-28 23:59:40 +0900

    220328
    스테이지 데이터 추가

--- Rhythm Jump/Assets/Scripts/StageDataManager.cs
+++ Rhythm Jump/Assets/Scripts/StageDataManager.cs
@@ -29,7 +29,7 @@ public class StageDataManager : MonoBehaviour
         stageDatas.Add(new StageData(5, 30, 3, 2, 2, 0.3f, new int[] { 5, 0, 6, 0, 5, 0, 6, 0, 0, 7, 0, 8, 0, 7, 0, 8, 0, 0, 11, 11, 0, 0 }, 1f));
         stageDatas.Add(new StageData(6, 30, 4, 1, 2, 0.3f, new int[] { 7, 0, 0, 8, 0, 0, 7, 0, 0, 8, 0, 0, 7, 0, 0, 8, 0, 0, 7, 0, 0, 8, 0, 0 }, 0.5f));
         stageDatas.Add(new StageData(7, 30, 3, 3, 2, 0.3f, new int[] { 7, 0, 7, 0, 7, 0, 8, 0, 8, 0, 8, 0, 7, 0, 7, 0, 7, 0, 8, 0, 8, 0, 8, 0, 9, 0, 0 }, 0));
-        stageDatas.Add(new StageData(8, 20, 3, 3, 2, 0.3f, new int[] { 9, 0, 10, 0, 9, 0, 10, 0, 11, 11, 11, 11, 11, 11, 11, 11, 0, 9, 0, 10, 0, 9, 0, 10, 0, 0 }, 0));
+        stageDatas.Add(new StageData(8, 20, 3, 3, 2, 0.3f, new int[] { 9, 0, 10, 0, 9, 0, 10, 0, 11, 11, 11, 11, 11, 11, 11, 11, 0, 0, 9, 0, 10, 0, 0 }, 0));
         stageDatas.Add(new StageData(9, 15, 3, 3, 2, 0.3f, new int[] { 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 0, 0 }, 0));
         stageDatas.Add(new StageData(10, 25, 2, 2, 1, 0.5f, new int[] { 5, 0, 7, 0, 6, 0, 8, 0, 5, 0, 7, 0, 6, 0, 8, 0, 5, 0, 7, 0, 6, 0, 8, 0, 5, 0, 7, 0, 6, 0, 8, 0, 1, 0, 2, 0, 3, 0, 4, 0, 0 }, 0));
         stageDatas.Add(new StageData(11, 10, 2, 3, 2, 0.25f, new int[] { 5, 0, 5, 0, 6, 0, 6, 0, 7, 0, 7, 0, 8, 0, 8, 0, 8, 0, 8, 0, 7, 0, 7, 0, 6, 0, 6, 0, 5, 0, 5, 0, 0 }, 0));
